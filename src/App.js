e56f05c55c2318c7c0ffecb27db1d2c53442b090
Owethu Sotomela <owethusotomela@gmail.com>
2024-04-25 20:30:09 +0200

    Added logic

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Home from './components/home/Home';
 
 function App() {
 
   const [movies, setMovies] = useState();
 
-  const getMovies = async () =>{
+  const getMovies = async () => {
 
-    try{
+    try {
 
       const response = api.get("/api/v1/movies");
 
       console.log(response.data)
 
       setMovies(response.data)
-      
-    }catch(err){
+
+    } catch (err) {
       console.log(err)
     }
 
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getMovies();
   }, []);
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = ({movies}) =>{
+    return(
+        <div>
+            Welcome Owethu Sotomela!
+        </div>
+    )
+}
+
+export default Home;
