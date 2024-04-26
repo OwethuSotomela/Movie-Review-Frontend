e9584aac6a0ba35aadfe41fee5adb6f8c4eb2294
Owethu Sotomela <owethusotomela@gmail.com>
2024-04-26 14:57:46 +0200

    Added more component,logic and style

--- src/components/trailer/Trailer.js
+++ src/components/trailer/Trailer.js
@@ -2,13 +2,15 @@ import { userParams } from "react-router-dom";
 import { ReactPlayer } from "react-player";
 import "./Trailer.css"
 
-const Trailer = ()=>{
+const Trailer = () => {
 
     let params = userParams();
-    let key = params.ytTrailerId; 
+    let key = params.ytTrailerId;
 
-    return(
-        <div className="react-player-container"></div>
+    return (
+        <div className="react-player-container">
+            {(key != null) ? <ReactPlayer controls="true" />}
+        </div>
     )
 }
 
