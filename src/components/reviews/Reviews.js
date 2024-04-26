3f3ad8d1ce75557e8253b7b4b99344f37f9ccbdc
Owethu Sotomela <owethusotomela@gmail.com>
2024-04-26 23:39:24 +0200

    Added reviews component

---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,92 @@
+import { useEffect, useRef } from "react";
+import { useParams, userParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+
+import api from "../../api/axiosConfig";
+import ReviewForm from "../reviewForm/reviewForm";
+
+const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
+
+    const revText = useRef();
+    let params = useParams();
+    const movieId = params.movieId;
+
+    useEffect(() => {
+        getMovieData(movieId);
+    }, []);
+
+    const addReview = async (e) => {
+
+        e.preventDefault();
+
+        const rev = revText.current;
+
+        try {
+
+        } catch (err) {
+            console.error(err)
+        }
+
+        const response = await api.post("/api/review/reviews", { reviewBody: rev.value, imdbId: movieId })
+
+        const updatedReviews = [...reviews, { body: rev.value }];
+
+        rev.value = "";
+
+        setReviews(updatedReviews);
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col><h3>Reviews</h3></Col>
+            </Row>
+            <Row className="mt-2">
+                <Col>
+                    <img src={movie?.poster} col="" />
+                </Col>
+                <Col>
+                    {
+                        <>
+                            <Row>
+                                <Col>
+                                    <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a Review?" />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <hr />
+                                </Col>
+                            </Row>
+                        </>
+                    }
+                    {
+                        reviews.map((r) => {
+                            return (
+                                <>
+                                    <Row>
+                                        <Col>
+                                            {r.body}
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <hr />
+                                        </Col>
+                                    </Row>
+                                </>
+                            )
+                        })
+                    }
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <hr />
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Reviews;
